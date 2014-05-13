12d93622d8375db687a1cc01ae4bed353986701a
yufeng217 <a356914617@gmail.com>
2014-05-13 11:29:40 +0800

    新增NormalService

---
+++ src/com/example/servicedemo/service/NormalService.java
@@ -0,0 +1,15 @@
+package com.example.servicedemo.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class NormalService extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
